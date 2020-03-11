be59348af8402b6ce37ba6d31806aad35bf46bc2
sharkylaa <59977705+sharkylaa@users.noreply.github.com>
2020-03-11 17:28:00 -0400

    Add files via upload

---
+++ socialstudies1.js
@@ -0,0 +1,51 @@
+/* [QUESTIONS, OPTIONS, ANSWERS] */
+//social studies questions
+// array that contains objects
+// format of {q: QUESTION, o: OPTIONS, a: CORRECT ANSWER}
+var questions = [
+  {
+    q : "How many states are in the United States?",
+    o : [
+      "40",
+      "10",
+      "20",
+      "50"
+    ],
+    a : 3
+  },
+  {
+    q : "What is the name of a region that is very hot and dry?",
+    o : [
+      "the South Pole",
+      "The Forest",
+      "The Desert"
+    ],
+    a : 2
+  },
+  {
+    q : "The compass on a map helps you know which _____ to go?",
+    o : [
+      "direction",
+      "habitat",
+      "leave"
+    ],
+    a : 0
+  },
+  {
+    q : "Rules help my family ______.",
+    o : [
+      "argue",
+      "be mean",
+      "get along"
+    ],
+    a : 2
+  },
+  {
+    q : "What did Thomas Edison invent?",
+    o : [
+      "the telephone",
+      "the phonograph"
+    ],
+    a : 1
+  }
+];
---
+++ socialstudies2.js
@@ -0,0 +1,52 @@
+/* [QUESTIONS, OPTIONS, ANSWERS] */
+//social studies2 questions
+// array that contains objects
+// format of {q: QUESTION, o: OPTIONS, a: CORRECT ANSWER}
+var questions = [
+  {
+    q : "Trees are used to build houses and make paper?",
+    o : [
+      "true",
+      "false"
+    ],
+    a : 0
+  },
+  {
+    q : "The largest continent is _____.",
+    o : [
+      "Asia",
+      "Antarctica",
+      "North America"
+    ],
+    a : 0
+  },
+  {
+    q : "What sport did Jack Robinson play",
+    o : [
+      "footbal",
+      "hockey",
+      "soccer",
+      "baseball"
+    ],
+    a : 3
+  },
+  {
+    q : "Florida is a _______.",
+    o : [
+      "state",
+      "city",
+      "country"
+    ],
+    a : 0
+  },
+  {
+    q : "What continent do we live on?",
+    o : [
+      "Asia",
+      "North America",
+      "Africa",
+      "Australia"
+    ],
+    a : 1
+  }
+];
